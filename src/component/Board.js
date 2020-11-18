80130ebcc38e3a6bd3b968d943d867ea0d4d5e6c
abhay singh <abhaysinghthakur.982@gmail.com>
2020-11-18 10:00:15 +0530

    added usestate and some events

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,28 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square'
 const Board = () => {
+
+    const [board, setboard] = useState(Array(9).fill(null));
+
+    const [isNext, setisNext] = useState(false);
+    console.log(board);
+
+    const handleSquareClick = (position) => {
+
+        if (board[position]) {
+            return;
+        }
+        setboard(prev => {
+
+            return prev.map((Square, pos) => {
+                console.log(Square)
+                if (pos === position) {
+                    return isNext ? '1' : '0';
+                }
+
+
+                return Square;
+            });
+
+        });
+
+        setisNext(prev => !prev);
+    };
+
+    const renderSquare = (position) => {
+        return (
+
+            <Square value={board[position]}
+                onClick={() => handleSquareClick(position)} />
+        )
+
+    }
     return (
         <div className="board">
             <div className="board-row">
-                <Square value={0} />
-                <Square value={1} />
-                <Square value={2} />
+
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+
+                {/* <Square value={board[0]} />
+                <Square value={board[1]} />
+                <Square value={board[2]} /> */}
             </div>
 
 
 
             <div className="board-row">
-                <Square value={3} />
-                <Square value={4} />
-                <Square value={5} />
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+
+                {/* <Square value={board[3]} />
+                <Square value={board[4]} />
+                <Square value={board[5]} /> */}
             </div>
 
 
 
             <div className="board-row">
-                <Square value={6} />
-                <Square value={7} />
-                <Square value={8} />
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+
+                {/* <Square value={board[6]} />
+                <Square value={board[7]} />
+                <Square value={board[8]} /> */}
 
             </div>
         </div>
--- src/component/Square.js
+++ src/component/Square.js
@@ -3,19 +3,21 @@ import React from 'react'
 
 // we have both option/////
 
-// const Square = ({ value }) => {
-//     return (
-
-//         <button type="button">{value}</button>
-//     )
-// }
-
-const Square = (props) => {
+const Square = ({ value, onClick }) => {
     return (
 
-        <button type="button" className="square">{props.value}</button>
+        <button type="button" className="square" onClick={onClick} >
+            { value}</button >
     )
 }
+
+// const Square = (props) => {
+//     return (
+
+//         <button type="button" className="square">
+//         {props.value}</button>
+//     )
+// }
 
 
 
